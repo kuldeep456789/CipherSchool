ee10b82c9d099d09fafbde47275026b2d73b9cfe
Kuldeep Prajapati <kuldeeppraj2002@gmail.com>
2024-08-24 01:20:54 +0530

    hello

---
+++ Lecture37/Solution.java
@@ -0,0 +1,19 @@
+package Lecture37;
+
+public class Solution
+{
+    public static boolean detectLoop(Node head)
+    {
+        Node slow = head;
+        Node fast = head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+} {
+    
+}
